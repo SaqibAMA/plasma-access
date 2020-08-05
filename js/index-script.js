4e692f70c6f24966f164de7a306c24cdc89cf9d1
Saqib Ali <business.saqib@gmail.com>
2020-08-05 14:03:43 +0500

    Implemented JSON based data display
    Added functionality to dynamically display list of donors using JSON data.
    Modified grid-sizing to support longer names.

--- js/index-script.js
+++ js/index-script.js
@@ -9,3 +9,36 @@ $(document).scroll(()=>{
     });
 
 });
+
+$(document).ready( ()=> {
+
+    $.getJSON('../sampleDonorData.json', (jsonData) => {
+
+        for (var i = 0; i < 2; i++) {
+
+            var dataHandle = jsonData['donors'][i];
+
+            var data = `
+            <li class="donorCardContainer">
+                <div class="donorCard">
+
+                    <div class="donorDetails">
+                    
+                        <div class="donorName">${dataHandle.name}</div>
+                        <div class="donorPhoneNum">${dataHandle.phoneNum}</div>
+                        <div class="donorCity">${dataHandle.city}</div>
+                        <div class="donorBloodGroup">${dataHandle.bloodGroup}</div>
+
+                    </div>
+                </div>
+            </li>`
+
+
+            $('#donorsList').append(data);
+
+
+        }
+
+    });
+
+});
